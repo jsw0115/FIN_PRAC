c8543329822721c3c4c338ae7c40172515776d4e
heoleejung01 <heoleejung01@naver.com>
2022-07-19 19:39:46 +0900

    20220717

--- app/src/main/java/org/tchtown/login/MainActivity.java
+++ app/src/main/java/org/tchtown/login/MainActivity.java
@@ -3,13 +3,23 @@ package org.tchtown.login;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.loader.content.AsyncTaskLoader;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,14 +37,7 @@ public class MainActivity extends AppCompatActivity {
 
         ///////////////////////////////////////////////////////////
         noticeListView = (ListView) findViewById(R.id.noticeListView);
-
-
-
-
         noticeList = new ArrayList<>();
-        noticeList.add(new notice("공지사항입니다.","안경잡이개발자","2022-01-01"));
-        noticeList.add(new notice("공지사항입니다.","안경잡이개발자","2022-01-01"));
-        noticeList.add(new notice("공지사항입니다.","안경잡이개발자","2022-01-01"));
         adapter = new NoticeListAdapter(getApplicationContext(), noticeList);
         noticeListView.setAdapter(adapter);
 
@@ -85,5 +88,69 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.commit();
             }
         });
+
+        new BackgroundTask().execute();
+    }
+
+    class BackgroundTask extends AsyncTask<Void, Void, String>{
+        String target;
+        @Override
+        protected void onPreExecute(){
+            target="http://jsw0101151.cafe24.com/NoticeList.php";
+        }
+
+        @Override
+        protected String doInBackground(Void... voids){
+            try
+            {
+                URL url = new URL(target);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String temp;
+                StringBuilder stringBuilder = new StringBuilder();
+                while((temp = bufferedReader.readLine()) != null)
+                {
+                    stringBuilder.append(temp+"\n");
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return stringBuilder.toString().trim();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        public void onProgressUpdate(Void... values){
+            super.onProgressUpdate();
+        }
+
+        @Override
+        public void onPostExecute(String result){
+            try{
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jsonArray = jsonObject.getJSONArray("response");
+                int count=0;
+                String noticeContent,noticeName,noticeDate;
+                while(count < jsonArray .length())
+                {
+                    JSONObject object = jsonArray.getJSONObject(count);
+                    noticeContent = object.getString("noticeContent");
+                    noticeName = object.getString("noticeName");
+                    noticeDate = object.getString("noticeDate");
+                    notice notice = new notice(noticeContent,noticeName,noticeDate);
+                    noticeList.add(notice);
+                    count++;
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+
+            }
+        }
     }
 }
